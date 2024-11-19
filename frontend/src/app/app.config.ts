ab06cf7886bff1dee8ea27cfaf24d8693576796b
Hugues <hugues.devimeux@epfl.ch>
2024-11-19 18:45:41 +0100

    Fixed incorrect basepath for API for production

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -5,8 +5,7 @@ export class AppConfig {
 
   //needs to be blank for regular deployment
   // private overwriteBasePath: string = ''; //'http://127.0.0.1:8080'
-   // private overwriteBasePath: string = ''; //'http://127.0.0.1:8080'
-  private overwriteBasePath: string = 'http://127.0.0.1:8080';
+    private overwriteBasePath: string = ''; //'http://127.0.0.1:8080'
 
   get basePath(): string {
     if (this.overwriteBasePath.length != 0) {

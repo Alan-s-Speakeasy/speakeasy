4f5db24701caeeec16ecadfd3bc5a47b2632e1af
hugues <36239975+huguesdevimeux@users.noreply.github.com>
2025-02-15 18:16:58 +0100

    Unselect all when switching view

--- frontend/src/app/user-feedback/user-feedback.component.ts
+++ frontend/src/app/user-feedback/user-feedback.component.ts
@@ -400,11 +400,14 @@ export class UserFeedbackComponent implements OnInit, OnDestroy {
   }
 
   toggleDirection(value: string): void {
+    // Unselect all usernames when changing the direction
+    this.selectedUsernames = [];
     this.authorPerspective = value == "author";
     this.fetchFeedback();
   }
 
   toggleAssignments(value: string): void {
+    this.selectedUsernames = [];
     this.chooseAssignments = value == "assigned";
     this.fetchFeedback();
   }

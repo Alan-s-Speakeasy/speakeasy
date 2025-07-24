96eb3437c73ea8162dbddd641722af2ad2a459d2
hugues <36239975+huguesdevimeux@users.noreply.github.com>
2025-07-24 16:04:55 +0200

    Moved harcoded URL to config files

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  basePath: 'https://files.ifi.uzh.ch/ddis/teaching/2021/ATAI/dataset/movienet/'
 };
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  basePath: 'https://files.ifi.uzh.ch/ddis/teaching/2021/ATAI/dataset/movienet/'
 };
 
 /*
